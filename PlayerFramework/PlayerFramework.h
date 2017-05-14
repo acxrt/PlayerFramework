9371daadf2517ebb955892433d3dcc9898b2f53b
Aina Cuxart <ainacuxart@gmail.com>
2017-05-15 01:02:35 +0200

    Initial Commit

---
+++ PlayerFramework/PlayerFramework.h
@@ -0,0 +1,19 @@
+//
+//  PlayerFramework.h
+//  PlayerFramework
+//
+//  Created by Aina Cuxart on 15/5/17.
+//  Copyright © 2017 Aina Cuxart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayerFramework.
+FOUNDATION_EXPORT double PlayerFrameworkVersionNumber;
+
+//! Project version string for PlayerFramework.
+FOUNDATION_EXPORT const unsigned char PlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerFramework/PublicHeader.h>
+
+
